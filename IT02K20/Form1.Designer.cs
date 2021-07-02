8abcfd809defc9b60827a80a7b875278ace0d074
HoangTN <HoangTN@DESKTOP-H68FGFI>
2021-07-02 08:57:57 +0700

    Update 07 thang 02 năm 2021

---
+++ IT02K20/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace IT02K20
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.txtDatabase = new System.Windows.Forms.TextBox();
+            this.txtUserID = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnWindows = new System.Windows.Forms.Button();
+            this.btnLoginSQL = new System.Windows.Forms.Button();
+            this.lblThongBao = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(174, 54);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(132, 20);
+            this.txtServer.TabIndex = 0;
+            // 
+            // txtDatabase
+            // 
+            this.txtDatabase.Location = new System.Drawing.Point(174, 89);
+            this.txtDatabase.Name = "txtDatabase";
+            this.txtDatabase.Size = new System.Drawing.Size(132, 20);
+            this.txtDatabase.TabIndex = 0;
+            // 
+            // txtUserID
+            // 
+            this.txtUserID.Location = new System.Drawing.Point(174, 126);
+            this.txtUserID.Name = "txtUserID";
+            this.txtUserID.Size = new System.Drawing.Size(132, 20);
+            this.txtUserID.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(174, 162);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(132, 20);
+            this.txtPassword.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(127, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "KẾT  NỐI CSDL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tên server";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CSDL";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "User ID";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(86, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Mật khẩu:";
+            // 
+            // btnWindows
+            // 
+            this.btnWindows.Location = new System.Drawing.Point(108, 231);
+            this.btnWindows.Name = "btnWindows";
+            this.btnWindows.Size = new System.Drawing.Size(75, 23);
+            this.btnWindows.TabIndex = 3;
+            this.btnWindows.Text = "Windows";
+            this.btnWindows.UseVisualStyleBackColor = true;
+            this.btnWindows.Click += new System.EventHandler(this.btnWindows_Click);
+            // 
+            // btnLoginSQL
+            // 
+            this.btnLoginSQL.Location = new System.Drawing.Point(218, 231);
+            this.btnLoginSQL.Name = "btnLoginSQL";
+            this.btnLoginSQL.Size = new System.Drawing.Size(75, 23);
+            this.btnLoginSQL.TabIndex = 4;
+            this.btnLoginSQL.Text = "Login SQL";
+            this.btnLoginSQL.UseVisualStyleBackColor = true;
+            this.btnLoginSQL.Click += new System.EventHandler(this.btnLoginSQL_Click);
+            // 
+            // lblThongBao
+            // 
+            this.lblThongBao.AutoSize = true;
+            this.lblThongBao.Location = new System.Drawing.Point(85, 196);
+            this.lblThongBao.Name = "lblThongBao";
+            this.lblThongBao.Size = new System.Drawing.Size(35, 13);
+            this.lblThongBao.TabIndex = 5;
+            this.lblThongBao.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 278);
+            this.Controls.Add(this.lblThongBao);
+            this.Controls.Add(this.btnLoginSQL);
+            this.Controls.Add(this.btnWindows);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserID);
+            this.Controls.Add(this.txtDatabase);
+            this.Controls.Add(this.txtServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.TextBox txtDatabase;
+        private System.Windows.Forms.TextBox txtUserID;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnWindows;
+        private System.Windows.Forms.Button btnLoginSQL;
+        private System.Windows.Forms.Label lblThongBao;
+    }
+}
+
---
+++ IT02K20/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IT02K20
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoginSQL_Click(object sender, EventArgs e)
+        {
+            string sqlConnectionString = "";
+            sqlConnectionString += "Data Source =" + txtServer.Text.Trim();
+            sqlConnectionString += ";Initial Catalog=" + txtDatabase.Text.Trim();
+            sqlConnectionString += ";User ID=" + txtUserID.Text.Trim();
+            sqlConnectionString += ";Password=" + txtPassword.Text.Trim();
+            SqlConnection connection = new SqlConnection(sqlConnectionString);
+            try
+            {
+                connection.Open();
+                lblThongBao.Text = "Kết nối thành công đến server " + txtServer.Text;
+
+            }
+            catch(SqlException ex)
+            {
+                lblThongBao.Text = "Kết nối thất bại đến " + txtServer.Text;
+            }
+            finally
+            {
+                connection.Dispose();
+            }
+
+        }
+
+        private void btnWindows_Click(object sender, EventArgs e)
+        {
+           string sqlConnectionString= @"Data Source= DESKTOP-H68FGFI\SQLSERVER;" +
+                "Initial Catalog = QLSV;" +
+                "Integrated Security = True";
+            SqlConnection connection = new SqlConnection(sqlConnectionString);
+
+            try
+            {
+                connection.Open();
+                lblThongBao.Text = "Kết nối thành công đến server " + txtServer.Text;
+
+            }
+            catch (SqlException ex)
+            {
+                lblThongBao.Text = "Kết nối thất bại đến " + txtServer.Text;
+            }
+            finally
+            {
+                connection.Dispose();
+            }
+        }
+    }
+}
