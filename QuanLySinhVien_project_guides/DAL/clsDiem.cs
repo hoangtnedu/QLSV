6c1d48c4c84b5496c4631ff100aa9e8adc325405
HoangTN <HoangTN@DESKTOP-H68FGFI>
2021-07-06 08:28:37 +0700

    update ngay 06 thang 07

--- QuanLySinhVien_project_guides/DAL/clsDiem.cs
+++ QuanLySinhVien_project_guides/DAL/clsDiem.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace QuanLySinhVien_project_guides.DAL
 {
-    class clsDiem
+    class clsDiem : IDiem
     {
+        public string MaSV { get; set; }
+        public string MaMH { get; set; }
+        public float DiemLan1 { get; set; }
+        public float DiemLan2 { get; set; }
+        public string GhiChu { get; set; }
+        public int Them1Diem()
+        {
+            return 0;
+        }
+        public int Xoa1Diem()
+        {
+            return 0;
+        }
+        public int Sua1Diem()
+        {
+            return 0;
+        }
     }
 }
