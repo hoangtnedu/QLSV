646ad584b1d445ef5127d14eb33e26f47f00a3b9
HoangTN <HoangTN@DESKTOP-H68FGFI>
2021-07-06 08:25:00 +0700

    update ngay 06 thang 07

---
+++ QuanLySinhVien_project_guides/DAL/IDiem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLySinhVien_project_guides.DAL
+{
+    interface IDiem
+    {
+        string MaSV { get; set; }
+        string MaMH { get; set; }
+        float DiemLan1 { get; set; }
+        float DiemLan2 { get; set; }
+        string GhiChu { get; set; }
+        int Them1Diem();
+        int Xoa1Diem();
+        int Sua1Diem();
+    }
+}
---
+++ QuanLySinhVien_project_guides/DAL/clsDiem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLySinhVien_project_guides.DAL
+{
+    class clsDiem
+    {
+    }
+}
