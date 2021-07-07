49613074c5bb713078125c69cfb461bd0bfb503d
HoangTN <HoangTN@DESKTOP-H68FGFI>
2021-07-07 10:31:03 +0700

    Update ngay 07 thang 07

---
+++ QuanLySinhVien_project_guides/Ultil/Ultil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLySinhVien_project_guides.Ultil
+{
+   public class Ultil
+    {
+        public bool ExcuteProcedure(string []paras, string []values,string strConnection,string sp_name)
+        {
+            bool kt = false;
+            SqlConnection connection = new SqlConnection(strConnection);
+            if (connection.State == ConnectionState.Closed) connection.Open();
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = sp_name;
+            command.CommandType = CommandType.StoredProcedure;
+            for (int i = 0; i < paras.Length; i++)
+            {
+                command.Parameters.AddWithValue(paras[i], values[i]);
+            }
+            try
+            {
+                command.ExecuteNonQuery();
+                kt = true;
+            }
+            catch (Exception)
+            {                
+            }    
+            
+            command.Dispose();
+            connection.Dispose();
+            return kt;
+
+        }
+    }
+}
